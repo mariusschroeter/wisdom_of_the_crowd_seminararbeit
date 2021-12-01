a1c9ad8e8654d0f305e0d1caa5463ab7f60d1f20
Marius Schröter <43613156+mariusschroeter@users.noreply.github.com>
2021-12-01 15:27:03 +0100

    Add files via upload

--- Auswertung.R
+++ Auswertung.R
@@ -19,121 +19,168 @@ other_league <- lsdf_100%>%
            competition != "Premier League" &
            competition != "Serie A")
 
+# -- Tabelle 1 --
+#siehe premier_league Zeile 3
+
 # -- Tabelle 2 --
 #Summe der Spiele
-bundesliga%>%
+lsdf_100%>%
   count()
 #Summe der Abstimmungen
-bundesliga%>%
+lsdf_100%>%
   summarise(sum(totalVotingCount))
 #Durchschnitt der Abstimmungen
 #Summe der Abstimmungen / Summe der Spiele
-#selbe herangehensweise für premier_league, serie_a und other_league Datensätze
+#selbe herangehensweise für bundesliga, premier_league, serie_a und 
+#other_league Datensätze
 
 # -- Tabelle 3 --
-bundesliga%>%
+lsdf_100%>%
   filter(homeGoals > awayGoals)%>%
   count()
-# 32/74 = 0.4324 = 43.24% (s.Tabelle 2: Zeile 1, Spalte 1)
-bundesliga%>%
+# 1681/3802 = 0,4421 = 44.21% (s.Tabelle 3: Zeile 5, Spalte 1)
+lsdf_100%>%
   filter(homeGoals == awayGoals)%>%
   count()
-# 18/74 = 0.2432 = 24.32%
-bundesliga%>%
+# 981/3802
+lsdf_100%>%
   filter(homeGoals < awayGoals)%>%
   count()
-# 24/74 = 0.3243 = 32.43%
-#selbe herangehensweise für premier_league, serie_a und other_league Datensätze
+# 1140/3802
+#selbe herangehensweise für bundesliga, premier_league, serie_a und 
+#other_league Datensätze
 
 # -- Tabelle 4 --
-bundesliga%>%
-  summarise(sum(homeOdd) / 74)
-#selbe herangehensweise für premier_league, serie_a, other_league
-#und lsdf_100 Datensätze
+lsdf_100%>%
+  summarise(sum(homeOdd) / 3802)
+#selbe herangehensweise für bundesliga, premier_league, serie_a und 
+#other_league Datensätze
 
-# -- Tabelle 5, 6, 7 und 8-- 
-#Am Beispiel der Bundesliga
+# -- Tabelle 5, 6, 7 --
+#siehe premier_league zeile 3 spalten homeVotingCount, drawVotingCount,
+#awayVotingCount und totalVotingCount
+
+
+
+# -- Tabelle 8-- 
+#Am Beispiel lsdf_100 Datensatz
 #Prozentsatz home/draw/away (Wie oft kam welches Ergebniss?)
-bundesliga%>%
+lsdf_100%>%
   mutate(ls_home_prop = homeVotingCount / totalVotingCount, 
          ls_draw_prop = drawVotingCount / totalVotingCount,
-         ls_away_prop = awayVotingCount / totalVotingCount) -> bundesliga
+         ls_away_prop = awayVotingCount / totalVotingCount) -> lsdf_100
 
 #Erwartungswert berechnen
-bundesliga%>%
+lsdf_100%>%
   mutate(ls_home_ev = ls_home_prop * homeOdd,
          ls_draw_ev = ls_draw_prop * drawOdd,
-         ls_away_ev = ls_away_prop * awayOdd) -> bundesliga
+         ls_away_ev = ls_away_prop * awayOdd) -> lsdf_100
 
 #Welcher Erwartungswert ist am höchsten? 1-heim, 0-draw, -1-away
-bundesliga%>%
+lsdf_100%>%
   mutate(ls_bet = if_else((ls_home_ev > ls_draw_ev) & (ls_home_ev > ls_away_ev), 1,
                           if_else((ls_draw_ev > ls_home_ev) & (ls_draw_ev > ls_away_ev), 0,
-                                  -1))) -> bundesliga
+                                  -1))) -> lsdf_100
 
 #Welches Team hat gewonnen? heimteam 1, draw 0, away -1
-bundesliga%>%
+lsdf_100%>%
   mutate(result = if_else(homeGoals > awayGoals,1,
-                             if_else(awayGoals > homeGoals, -1, 0))) -> bundesliga
+                             if_else(awayGoals > homeGoals, -1, 0))) -> lsdf_100
 
 #Wieviel Profit macht man bei einem Euro Einsatz?
-bundesliga%>%
+lsdf_100%>%
   mutate(ls_profit_random = if_else(result == ls_bet_random, 
                              if_else(result == 1, homeOdd, 
                                      if_else(result == 0, drawOdd,
-                                             awayOdd)), -1)) -> bundesliga
-
-#Modell des reinen Zufalls (Jedes mal anders)
-bundesliga%>%
-  mutate(ls_bet_random = sample(-1:1, 3559, replace = TRUE)) -> bundesliga
-
-#Heimteam gewinnt Modell
-bundesliga%>%
-  mutate(ls_profit_homeWin = if_else(result == 1, homeOdd, -1)) -> bundesliga
-
-#Wettquoten Modell
-bundesliga%>%
-  mutate(ls_bet_odds = if_else((homeOdd < drawOdd & homeOdd < awayOdd), 1,
-                               if_else((awayOdd < homeOdd) & (awayOdd < drawOdd), -1,
-                                       0))) -> bundesliga
+                                             awayOdd)), -1)) -> lsdf_100
 
 #Alternative -> Ich wette garnicht
-bundesliga%>%
-  mutate(no_bet_profit = 0) -> bundesliga
+lsdf_100%>%
+  mutate(no_bet_profit = 0) -> lsdf_100
 
 #Profit von WdV
-bundesliga%>%
+lsdf_100%>%
  summarise(sum(ls_profit))
 
-#Profit von Zufalls Modell
-bundesliga%>%
-  summarise(sum(ls_profit_random))
+#Signifikant Unterschiedlich von 0?
+t.test(lsdf_100$ls_profit, lsdf_100$no_bet_profit, alternative = 'greater')
 
-#Profit von Heimteam gewinnt Modell
-bundesliga%>%
-  summarise(sum(ls_profit_homeWin))
+# -- Tabelle 9, 10, 11 --
+#Wettquoten Modell
+lsdf_100%>%
+  mutate(ls_bet_odds = if_else((homeOdd < drawOdd & homeOdd < awayOdd), 1,
+                               if_else((awayOdd < homeOdd) & (awayOdd < drawOdd), -1,
+                                       0))) -> lsdf_100
 
-#Profit von Heimteam gewinnt Modell
-bundesliga%>%
-  summarise(sum(ls_profit_odds))
+#Wettquoten Modell besser als mein Modell?
+t.test(lsdf_100$ls_profit_odds, lsdf_100$ls_profit, paired=TRUE, alternative = 'greater')
+#pwert ist sehr klein -> Wettquoten Modell besser als WdV-Modell!
 
-#Signifikants Test (Tabelle 5)
-#h0 -> resultat bei abstimmungen ist 0 
-#h1 -> resultat bei abstimmungen ist ungleich 0
-t.test(bundesliga$ls_profit, bundesliga$no_bet_profit, alternative = 'two.sided')
-t.test(bundesliga$ls_profit_random, bundesliga$no_bet_profit, alternative = 'two.sided')
-t.test(bundesliga$ls_profit_homeWin, bundesliga$no_bet_profit, alternative = 'two.sided')
-t.test(bundesliga$ls_profit_odds, bundesliga$no_bet_profit, alternative = 'two.sided')
+#Durchschnittsmodell (Tabelle 10)
+#vergleich mit dem Modell wie oft kommt welches Ergebnis vor.
+#Durchschnitte sind 44,14 25,43 und 30,43 (Tabelle 3)
+#quote * durchschnitte -> neuer EW
+#Erwartungswert berechnen
+lsdf_100%>%
+  mutate(ls_prop_home_ev = 0.44 * homeOdd,
+         ls_prop_draw_ev = 0.25 * drawOdd,
+         ls_prop_away_ev = 0.3 * awayOdd) -> lsdf_100
 
-#Signifikants Test Vergleich der Modelle (Tabelle 6, 7 und 8)
+#Welcher Erwartungswert ist am höchsten? 1-heim, 0-draw, -1-away
+lsdf_100%>%
+  mutate(ls_prop_bet = if_else((ls_prop_home_ev > ls_prop_draw_ev) & (ls_prop_home_ev > ls_prop_away_ev), 1,
+                          if_else((ls_prop_draw_ev > ls_prop_home_ev) & (ls_prop_draw_ev > ls_prop_away_ev), 0,
+                                  -1))) -> lsdf_100
 
-#besser als reiner Zufall?
-t.test(bundesliga$ls_profit, bundesliga$ls_profit_random, alternative = 'greater')
+#Wieviel Profit macht man bei einem Euro Einsatz?
+lsdf_100%>%
+  mutate(ls_profit_prop = if_else(result == ls_prop_bet, 
+                                    if_else(result == 1, homeOdd, 
+                                            if_else(result == 0, drawOdd,
+                                                    awayOdd)), -1)) -> lsdf_100
+#Wie erfolgreich ?
+t.test(lsdf_100$ls_profit_odds, lsdf_100$ls_profit_prop,paired = TRUE, alternative = 'greater')
+# Wettquoten Modell ist besser als das Durchschnittsmodell!
 
-#Heimteam sieg
-t.test(bundesliga$ls_profit, bundesliga$ls_profit_homeWin, alternative = 'greater')
+#3. Modell -> Die Underdogs sind unterbewertet?
+#Die Wahrscheinlichkeit für den vermeintlichen Verlierer wird + 5% genommen.
+lsdf_100%>%
+  mutate(
+    ls_ud_home_ev = if_else((homeOdd > awayOdd),
+                            homeOdd * (1 / homeOdd + 0.05),
+                            homeOdd * (1 / homeOdd - 0.025),
+                            ),
+    ls_ud_draw_ev = drawOdd * (1 / drawOdd - 0.025),
+    ls_ud_away_ev = if_else((awayOdd > homeOdd),
+                            awayOdd * (1 / awayOdd + 0.05),
+                            awayOdd * (1 / awayOdd - 0.025),
+    ),
+  ) -> lsdf_100
 
+#Welcher Erwartungswert ist am höchsten? 1-heim, 0-draw, -1-away
+lsdf_100%>%
+  mutate(ls_ud_bet = if_else((ls_ud_home_ev > ls_ud_draw_ev) & (ls_ud_home_ev > ls_ud_away_ev), 1,
+                               if_else((ls_ud_draw_ev > ls_ud_home_ev) & (ls_ud_draw_ev > ls_ud_away_ev), 0,
+                                       -1))) -> lsdf_100
+
+#Wieviel Profit macht man bei einem Euro Einsatz?
+lsdf_100%>%
+  mutate(ls_profit_ud = if_else(result == ls_ud_bet, 
+                                  if_else(result == 1, homeOdd, 
+                                          if_else(result == 0, drawOdd,
+                                                  awayOdd)), -1)) -> lsdf_100
+
+#Wie erfolgreich ?
+t.test(lsdf_100$ls_profit_odds, lsdf_100$ls_profit_ud, paired = TRUE,alternative = 'greater')
+# Das Wettquoten Modell ist besser als das Außenseiter Modell
+#
+
+# Auflistung der Ergebnisse
 #Wettquoten
-t.test(bundesliga$ls_profit, bundesliga$ls_profit_odds, alternative = 'greater')
-
-#gleiches Spiel für die anderen Datensätze (premier_league, serie_a, other_league, lsdf_100)
+sum(lsdf_100$ls_profit_odds)
+#WdV
+sum(lsdf_100$ls_profit)
+#Durchschnitt
+sum(lsdf_100$ls_profit_prop)
+#Außenseiter
+sum(lsdf_100$ls_profit_ud)
